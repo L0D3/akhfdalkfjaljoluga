1de8356ffa8249810f3dd2e493753906f2d0125b
johannes <johannes@johannes-lap.(none)>
2011-07-24 19:02:54 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PatverV1::Application.config.secret_token = '97a5af4ec69e522b0afa22e43eb95698388f384426eaaf61ebdd885ebc2ea861aab9bd9b8d0e7277017d6d8bcc6908318ad61b0b865ba272ba1031aed9ac86e9'
